7af834deeb6e4b6e7d6c88c7cad12b1dab7b7fa9
Antonio Fonseca <tonybaang@gmail.com>
2019-11-23 08:40:20 -0600

    Practica 5 - Prioridades

---
+++ Practica5/src/org/CUCEI/TSO/Practica5/Model/CPU.java
@@ -0,0 +1,76 @@
+package org.CUCEI.TSO.Practica5.Model;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.CUCEI.TSO.Practica5.UI.MainFrame;
+
+public class CPU implements Runnable {
+
+	private List<SOProcess> processList;
+	private List<SOProcess> processListtmp;
+
+	MainFrame mainFrame;
+
+	public CPU(List<SOProcess> processList, List<SOProcess> processListtmp, MainFrame mainFrame) {
+		this.processList = processList;
+		this.processListtmp = processListtmp;
+		this.mainFrame = mainFrame;
+	}
+
+	@Override
+	public void run() {
+		print("Iniciando CPU");
+		int arrivalTime = 0;
+		for (; arrivalTime < processList.size() / 4; arrivalTime++) {
+			processListtmp.add(processList.get(arrivalTime));
+			processListtmp.get(arrivalTime).setArrivalTime(arrivalTime);
+			processList.remove(arrivalTime);
+		}
+		while (true) {
+			for (SOProcess process : processListtmp) {
+				if (!(process.getStatus() == process.STATUS_BLOQUED || process.getStatus() == process.STATUS_FINISHED))
+					if (process.getStatus() == process.STATUS_NEW) {
+						print("Cambiando satus de: " + process.getName());
+						process.setStatus(process.STATUS_READY);
+						mainFrame.addRows();
+					}
+			}
+			sortProcessByPriority(processListtmp);
+			for (SOProcess process : processListtmp) {
+				if (process.getStatus() == process.STATUS_BLOQUED) {
+					break;
+				}
+				if (!(process.getStatus() == process.STATUS_BLOQUED || process.getStatus() == process.STATUS_FINISHED))
+					if (process.getStatus() == process.STATUS_READY) {
+						print("Executando: " + process.getName());
+						process.setStatus(process.STATUS_EXECUTING);
+						mainFrame.addRows();
+						process.run();
+						if (process.getBurstTime() < 0) {
+							process.setBurstTime(0);
+							process.setStatus(process.STATUS_FINISHED);
+						} else {
+							process.setStatus(process.STATUS_READY);
+						}
+						mainFrame.addRows();
+						// break;
+					}
+			}
+
+			if (arrivalTime < processList.size()) {
+				processListtmp.add(processList.get(arrivalTime));
+				processList.remove(arrivalTime);
+			}
+		}
+	}
+
+	private void sortProcessByPriority(List<SOProcess> processList) {
+		Collections.sort(processList);
+	}
+
+	private void print(String string) {
+		System.out.println("*_>" + string);
+	}
+
+}
---
+++ Practica5/src/org/CUCEI/TSO/Practica5/UI/MainFrame.java
@@ -0,0 +1,101 @@
+package org.CUCEI.TSO.Practica5.UI;
+
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.Label;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import org.CUCEI.TSO.Practica5.Model.SOProcess;
+
+public class MainFrame {
+
+	private JFrame mainFrame;
+	private List<SOProcess> processListtmp;
+	DefaultTableModel processTableModel;
+
+	public MainFrame(List<SOProcess> processListtmp, int quantum) {
+ 		this.processListtmp = processListtmp;
+		mainFrame = new JFrame("TSO - Prioridades - Jose Antonio Fonseca - Practica 5");
+		mainFrame.setSize(600, 400);
+ 		mainFrame.setLocationRelativeTo(null);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel quantumPanel = new JPanel();
+		JPanel tablePanel = new JPanel();
+		quantumPanel.add(new Label("Quantum - " + quantum));
+		JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setViewportView(getSOProcessTable());
+		tablePanel.add(scrollPane);
+		mainFrame.setLayout(new FlowLayout());
+		mainFrame.add(quantumPanel);
+		mainFrame.add(tablePanel);
+		mainFrame.setVisible(true);
+	}
+
+	public DefaultTableModel getTableModel() {
+		return processTableModel;
+	}
+
+	private Component getSOProcessTable() {
+		createTableModel();
+		addRows();
+		JTable processTable = new JTable(processTableModel);
+		processTable.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				int row = processTable.rowAtPoint(e.getPoint());
+				int column = processTable.columnAtPoint(e.getPoint());
+				if ((row > -1) && (column > -1)) {
+					popupProcess(row, column);
+				}
+			}
+
+			private void popupProcess(int row, int column) {
+			 new ProcessDialog(row, processListtmp, mainFrame);
+
+			}
+		});
+		JScrollPane tablePane = new JScrollPane(processTable);
+		return tablePane;
+	}
+
+	private void createTableModel() {
+		processTableModel = new DefaultTableModel() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		processTableModel.addColumn("Id");
+		processTableModel.addColumn("Name");
+		processTableModel.addColumn("Arrival Time");
+		processTableModel.addColumn("Status");
+		processTableModel.addColumn("Burst Time");
+		processTableModel.addColumn("Priority");
+	}
+
+	public void addRows() {
+		try {
+			if (processTableModel.getRowCount() > 0) {
+				for (int i = processTableModel.getRowCount(); i != 0; i--) {
+					processTableModel.removeRow(i - 1);
+				}
+			}
+		}
+		catch(Exception e) {}
+		for (SOProcess soProcess : processListtmp) {
+			Object[] obj = { soProcess.getId(), soProcess.getName(), soProcess.getArrivalTime(), soProcess.getStatusValue(), soProcess.getBurstTime(), soProcess.getPriority() };
+			processTableModel.addRow(obj);
+		}
+		processTableModel.fireTableDataChanged();
+	}
+
+}
